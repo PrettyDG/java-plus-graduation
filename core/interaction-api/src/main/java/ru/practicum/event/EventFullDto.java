ff9480cd09030b3aba8dc5323545ef9aeb7330c1
PrettyDG <danilapr11@yandex.ru>
2025-08-02 03:47:41 +0300

    try delete JsonFormat

--- core/interaction-api/src/main/java/ru/practicum/event/EventFullDto.java
+++ core/interaction-api/src/main/java/ru/practicum/event/EventFullDto.java
@@ -14,17 +14,17 @@ public class EventFullDto {
     private String annotation;
     private CategoryDto category;
     private Integer confirmedRequests;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+
     private LocalDateTime createdOn;
     private String description;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+
     private LocalDateTime eventDate;
     private Long id;
     private UserDto initiator;
     private LocationDto location;
     private Boolean paid;
     private Integer participantLimit;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+
     private LocalDateTime publishedOn;
     private Boolean requestModeration;
     private EventState state;
