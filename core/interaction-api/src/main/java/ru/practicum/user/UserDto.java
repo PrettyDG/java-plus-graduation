9b931f03e7fe4833d1c658099bd89c984a3236ab
PrettyDG <danilapr11@yandex.ru>
2025-08-03 13:59:05 +0300

    UserDto validation

--- core/interaction-api/src/main/java/ru/practicum/user/UserDto.java
+++ core/interaction-api/src/main/java/ru/practicum/user/UserDto.java
@@ -2,6 +2,7 @@ package ru.practicum.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -15,9 +16,11 @@ public class UserDto {
     private Long id;
 
     @NotBlank(message = "Name must not be blank")
+    @Size(min = 2, max = 250)
     private String name;
 
     @Email(message = "Email should be valid")
     @NotBlank(message = "Email must not be blank")
+    @Size(min = 6, max = 254)
     private String email;
 }
