ec85d6e0f63a8b87ff3c3a1e21aa1f7acc5f0e3f
PrettyDG <danilapr11@yandex.ru>
2025-08-03 14:05:37 +0300

    UserDto validation

--- core/interaction-api/src/main/java/ru/practicum/event/EventFullDto.java
+++ core/interaction-api/src/main/java/ru/practicum/event/EventFullDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.event;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import ru.practicum.category.CategoryDto;
 import ru.practicum.user.UserDto;
@@ -19,7 +20,7 @@ public class EventFullDto {
 
     private LocalDateTime createdOn;
     private String description;
-
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime eventDate;
     private Long id;
     private UserDto initiator;
